29d9c97e6a4b53de0d62bdc14e047f4e8075f338
zuhra sofyan <zuhrasofyan@gmail.com>
2020-09-14 10:12:38 +0200

    Add initial helloworld test route

---
+++ api/controllers/HalloController.js
@@ -0,0 +1,15 @@
+/**
+ * HalloController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+
+  hallo: function(req, res) {
+    return res.send('Hi, how are you :)');
+  }
+
+};
+
--- config/routes.js
+++ config/routes.js
@@ -10,5 +10,6 @@
 
 module.exports.routes = {
 
+  'GET /' : 'HalloController.hallo',
 
 };
