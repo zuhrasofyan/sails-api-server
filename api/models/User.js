5645cdc32133aa2e9cdecc50a30d7b93a311c741
zuhra sofyan <zuhrasofyan@gmail.com>
2020-09-15 08:57:51 +0200

    implement encrypt of user password

--- api/models/User.js
+++ api/models/User.js
@@ -5,27 +5,19 @@
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
  */
 
+const bcrypt = require('bcrypt-nodejs');
 module.exports = {
 
   attributes: {
 
-    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
-    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
-    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
-
-
-    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
-    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
-    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
-
-
-    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
-    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
-    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
     email: {
       type: 'string',
       required: true,
       unique: true,
+    },
+    username: {
+      type: 'string',
+      required: true,
     },
     password: {
       type: 'string',
@@ -33,6 +25,24 @@ module.exports = {
     }
 
   },
+
+  // a sailsjs feature to omit specific attribute in object. in this case, return without password. and Fd of files
+  // see https://sailsjs.com/documentation/concepts/models-and-orm/model-settings#?customtojson
+  customToJSON: function() {
+    return _.omit(this, ['password']);
+  },
+  beforeCreate: function(user, cb) {
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) {
+        return cb(err);
+      }
+      bcrypt.hash(user.password, salt, null, (err, hash) => {
+        if (err) {return cb(err);}
+        user.password = hash;
+        return cb();
+      });
+    });
+  }
 
 };
 
